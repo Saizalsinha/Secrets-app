fbe302f48a158682c5adc8bfd5e6f2f5bff290f2
Saizalsinha <saizalsinha2003@gmail.com>
2023-01-18 20:10:55 +0530

    Hash passwords

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -22,12 +23,10 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDb",
   console.log(err);
 })
 
-const userSchema = new mongoose.Schema({
+const userSchema ={
   email:String,
   password:String
-});
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+};
 
 const User  = new mongoose.model("User",userSchema);
 
@@ -46,7 +45,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
   const newUser = new User({
     email:req.body.username,
-    password:req.body.password
+    password:md5(req.body.password)
   });
   newUser.save(function(err){
     if(err)
@@ -58,7 +57,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email:username},function(err,foundUser){
     if(err)
